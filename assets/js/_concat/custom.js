ee3371258e678b74e63c93084b5df8fa7755528f
Dominick Washburn <domwashburn@gmail.com>
2014-07-09 20:18:47 -0700

    I abandoned the fancy auto-generated thumbnail business in lieu of something that actually works properly.

--- assets/js/_concat/custom.js
+++ assets/js/_concat/custom.js
@@ -2,9 +2,8 @@ $(document).ready (function(){
 	var videoBkg = function() {
 		var docHeight = $(document).height();
 		var docWidth = $(window).width();
-			$('.videoBackground').css('height', docHeight);
-			$('.videoBackground').css('width', docWidth);
-			console.log('it works')
+		$('.videoBackground').css('height', docHeight);
+		$('.videoBackground').css('width', docWidth);
 	};
 
 videoBkg();
